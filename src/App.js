a791e3f9f8381b77928b480b6fcb18ee1be954df
Rizkieawan <rizkieawan@Rizkieawans-MBP.lan>
2023-09-01 22:46:51 +0700

    change logo and add lubang buaya branch

--- src/App.js
+++ src/App.js
@@ -3,70 +3,116 @@ import { FaTiktok } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
 import { BsInstagram } from "react-icons/bs";
 import priceList from "../src/assets/naels-price.jpeg";
+import logo from "../src/assets/naels-logo.png";
 import { useState } from "react";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
+
+  const [isBookOptionOpen, setIsBookOptionOpen] = useState(false);
+  const [isLocationOptionOpen, setIsLocationOptionOpen] = useState(false);
 
   return (
     <div className="App">
       <div className="container">
         <div>
           <div className="wrapper">
-            <div className="logo">naels</div>
+            {/* <div className="logo">naels</div> */}
+            <img src={logo} alt="naels-mates" width={"70%"} />
             <div>your nails soulmate</div>
           </div>
 
           <div className="menus">
             <div
               className="item"
-              onClick={() => window.open("https://wa.me/6287703032019?text=")}
+              onClick={() => setIsBookOptionOpen(!isBookOptionOpen)}
             >
               Book Appointment
             </div>
+            {isBookOptionOpen && (
+              <div className="optionsWrapper">
+                <div
+                  className="options"
+                  onClick={() =>
+                    window.open("https://wa.me/6287703032019?text=")
+                  }
+                >
+                  Tebet
+                </div>
+                <div
+                  className="options"
+                  onClick={() =>
+                    window.open("https://wa.me/6282226661794?text=")
+                  }
+                >
+                  Lubang Buaya
+                </div>
+              </div>
+            )}
             <div className="item" onClick={() => setIsOpen(true)}>
               Price List
             </div>
             <div
               className="item"
-              onClick={() =>
-                window.open(
-                  "https://www.google.com/maps/place/Jl.+Pal+Batu+1+No.39,+RT.4%2FRW.4,+Menteng+Dalam,+Kec.+Tebet,+Kota+Jakarta+Selatan,+Daerah+Khusus+Ibukota+Jakarta+12870/@-6.227765,106.8443363,17z/data=!3m1!4b1!4m6!3m5!1s0x2e69f3921748e01d:0x54c60c14d125da02!8m2!3d-6.227765!4d106.846525!16s%2Fg%2F11csgfb3y_",
-                  "_blank"
-                )
-              }
+              onClick={() => setIsLocationOptionOpen(!isLocationOptionOpen)}
             >
               Location
             </div>
+            {isLocationOptionOpen && (
+              <div className="optionsWrapper">
+                <div
+                  className="options"
+                  onClick={() =>
+                    window.open(
+                      "https://www.google.com/maps/place/Jl.+Pal+Batu+1+No.39,+RT.4%2FRW.4,+Menteng+Dalam,+Kec.+Tebet,+Kota+Jakarta+Selatan,+Daerah+Khusus+Ibukota+Jakarta+12870/@-6.227765,106.8443363,17z/data=!3m1!4b1!4m6!3m5!1s0x2e69f3921748e01d:0x54c60c14d125da02!8m2!3d-6.227765!4d106.846525!16s%2Fg%2F11csgfb3y_",
+                      "_blank"
+                    )
+                  }
+                >
+                  Tebet
+                </div>
+                <div
+                  className="options"
+                  onClick={() =>
+                    window.open(
+                      "https://goo.gl/maps/meM2ke5exg8W5Cnp6",
+                      "_blank"
+                    )
+                  }
+                >
+                  Lubang Buaya
+                </div>
+              </div>
+            )}
           </div>
 
           <div className="social">
             <FaTiktok
-              color="white"
+              color="black"
               size={30}
               onClick={() =>
                 window.open("https://www.tiktok.com/@naelsmates", "_blank")
               }
             />
-            <SiGmail
-              color="white"
+            {/* <SiGmail
+              color="black"
               size={30}
               onClick={() => window.open("mailto:naelsmates@gmail.com")}
             />
-
+  
             <BsInstagram
-              color="white"
+              color="black"
               size={30}
               onClick={() =>
                 window.open("https://www.instagram.com/naelsmates__/")
               }
-            />
+            /> */}
           </div>
         </div>
 
-        <div className="footer">
+        {/* <div className="footer">
           <text>life is too short for boring nails</text>
-        </div>
+        </div> */}
       </div>
 
       {isOpen && (
