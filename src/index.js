c05cb821b65aa78accc11f3a97118b4eda085625
Rizkieawan <rizkieawan@Rizkieawans-MacBook-Pro.local>
2023-09-01 23:02:11 +0700

    add analytics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Helmet } from "react-helmet";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -12,9 +13,13 @@ root.render(
       <meta name="keywords" content="Naels, React.js, metadata" />
       <meta property="og:title" content="Naels" />
       <meta property="og:type" content="website" />
-      <meta property="og:description" content="life is too short for boring nails" />
+      <meta
+        property="og:description"
+        content="life is too short for boring nails"
+      />
       <meta property="og:url" content="https://naels.vercel.app/" />
     </Helmet>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
